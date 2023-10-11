d17d9a5589ba2a6742be99822d2605bed1323a0e
Abhishek Kumar Singh <79246354+mrsuspecious@users.noreply.github.com>
2023-10-11 15:51:08 +0530

    Add files via upload

---
+++ 17_calculator.py
@@ -0,0 +1,19 @@
+print("------MINI CALCULATOR------")
+first=input("Enter your first number: ")
+operator=input("Enter your operator(+,-,*,/,%): ")
+second=input("Enter your second number: ")
+first=int(first)
+second=int(second)
+
+if operator == "+":
+    print(first + second)
+elif operator == "-":
+    print(first - second)
+elif operator == "*":
+    print(first * second)
+elif operator == "/":
+    print(first / second)
+elif operator == "%":
+    print(first % second)
+else:
+    print("Invalid operator")
